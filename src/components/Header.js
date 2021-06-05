9bbb7a4b1e4e95a8438202607b00d987150369f4
QuantumCode2000 <tancaraninajorgeluis@gmail.com>
2021-06-04 20:08:13 -0400

    MEJORANDO LOS ESTILOS DE BOTONES Y ELIMINANDO ERRORES

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,26 +6,24 @@ export default function Header() {
         <div>
             <header >
                 <nav>
-                <ul className="nav-right-section">
-                    <li>
-                    <a href="https://mail.google.com/mail/u/0/?ogbl#inbox">Gmail</a>
-                    </li>
-                    <li>
-                    <a href="https://www.google.com.bo/imghp?hl=es-419&tab=ri&ogbl">Imagenes</a>
-                    </li>
-                    <li className="menu-icon">
-                    <a href="">
-                    </a>
-                    </li>
-                    <li>
-                    <a href="">
-                        <img  src="https://lh3.googleusercontent.com/ogw/ADGmqu9bOanDPtMya-JZKHqum8A-shdBxF_X10G9VTGd=s32-c-mo" alt="image profile" />
-                    </a>
-                    </li>
-                </ul>
+                    <ul className="nav-right-section">
+                        <li>
+                            <a href="https://mail.google.com/mail/u/0/?ogbl#inbox">Gmail</a>
+                        </li>
+
+                        <li>
+                            <a href="https://www.google.com.bo/imghp?hl=es-419&tab=ri&ogbl">Imagenes</a>
+                        </li>
+
+                        <li className="menu-icon">
+                        </li>
+
+                        <li className="img-profile">
+                            <img  src="https://lh3.googleusercontent.com/ogw/ADGmqu9bOanDPtMya-JZKHqum8A-shdBxF_X10G9VTGd=s32-c-mo" alt="profile" />
+                        </li>
+                    </ul>
                 </nav>
             </header>
-        </div>
-        
-    )
+        </div> 
+    );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -12,10 +12,11 @@ export default function Main() {
                 <section className="main-input" >
                     <div className="main-input-container">
                         <span className="search-icon" >
-                             
                         </span>
                         <input type="text" />
-                        <a className="micro-icon" href=""></a>
+                        <a className="micro-icon" href="/">
+                            <span></span>
+                        </a>
                     </div>
                 </section>
                 <section className="main-buttons">
